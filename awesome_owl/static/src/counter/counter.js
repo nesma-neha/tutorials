cd8e00ef97765e96e3edb66d352f69e5fa8bcb41
nesma-neha <neha@odoo.com>
2024-01-30 14:07:52 +0200

    added the sum of two counters

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -3,12 +3,17 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.counter";
+    static props = {
+        onChange: {type: Function, optional: true}
+    };
 
     setup() {
         this.state = useState({ value: 0});
     }
 
     increment() {
-        this.state.value = this.state.value + 1;
+        this.state.value++;
+        if(this.props.onChange) this.props.onChange();
     }
+
 }
